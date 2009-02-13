a06f4a9d0b6d22078d849c01c6eaf5f143564430
alex952.anf5 <alex952.anf5@ec0a5c5c-f22e-11dd-a295-0b7b0935bf94>
2009-02-13 11:41:57 +0000

    Funcion Buscar

--- trunk/src/tad.cpp
+++ trunk/src/tad.cpp
@@ -4,50 +4,42 @@ using namespace std;
 #include "tavlcom.h"
 
 int main ()
-{
+{	
+	TComplejo c1 (5,5), c2 (90,90), c3 (83,83), c4 (28,28), c5 (67,67),
+	c6 (81,81), c7 (50,50), c8 (43,43), c9 (55,55), c10(45,45),c_inventao(69,69);
 	TAVLCom a;
 	
-	for (int i=0; i<10000; i++)
-	{
-		TComplejo c (i,i);
-		
-		a.Insertar (c);
-	}
+	a.Insertar (c1);
+	a.Insertar (c2);
+	a.Insertar (c3);
+	a.Insertar (c4);
+	a.Insertar (c5);
+	a.Insertar (c6);
+	a.Insertar (c7);
+	a.Insertar (c8);
+	a.Insertar (c9);
+	a.Insertar (c10);
 	
-	//~ cout << "Preorden: " << a.Preorden () << endl;
-	//~ cout << "Postorder: " << a.Postorden () << endl;
-	//~ cout << "Inorden: " << a.Inorden () << endl;
-	cout << "Niveles: " << a.Niveles () << endl;
+	TAVLCom b(a);
 	
-	//~ TComplejo c1 (5,5), c2 (90,90), c3 (83,83), c4 (28,28), c5 (67,67),
-	//~ c6 (81,81), c7 (50,50), c8 (43,43), c9 (55,55), c10(45,45);
-	//~ TAVLCom a;
-	//~ 
-	//~ a.Insertar (c1);
-	//~ a.Insertar (c2);
-	//~ a.Insertar (c3);
-	//~ a.Insertar (c4);
-	//~ a.Insertar (c5);
-	//~ a.Insertar (c6);
-	//~ a.Insertar (c7);
-	//~ a.Insertar (c8);
-	//~ a.Insertar (c9);
-	//~ a.Insertar (c10);
-	//~ 
-	//~ TAVLCom b(a);
-	//~ 
-	//~ cout << "Inordena: " << a.Inorden () << endl;
-	//~ 
-	//~ cout << "Inordenb: " << b.Inorden () << endl;
-	//~ 
-	//~ cout << "Nivelesa: " << a.Niveles () << endl;
+	cout << "Inordena: " << a.Inorden () << endl;
 	
-	//~ cout << "Nodos: " << a.Nodos () << endl;
+	cout << "Inordenb: " << b.Inorden () << endl;
 	
-	//~ cout << "Nodos Hoja: " << a.NodosHoja () << endl;
+	cout << "Nivelesa: " << a.Niveles () << endl;
 	
-	//~ a.Borrar (c5);
+	cout << "Nodos: " << a.Nodos () << endl;
 	
-	//~ cout << "Preorden: " << a.Preorden () << endl;
+	cout << "Nodos Hoja: " << a.NodosHoja () << endl;
+	
+	if (a==b)	cout << "Son Iguales" << endl;
+	
+	a.Borrar (c5);
+	
+	cout << "Preorden: " << a.Preorden () << endl;
+	
+	if (a.Buscar (c10))	cout << "Encuentra el c10" << endl;
+	
+	if (!a.Buscar (c_inventao))	cout << "No encuentra el inventao" << endl;
 
 }
