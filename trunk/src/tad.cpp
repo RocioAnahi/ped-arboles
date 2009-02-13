749c87133b5d025c78e1bda531da7184ac5d26ab
alex952.anf5 <alex952.anf5@ec0a5c5c-f22e-11dd-a295-0b7b0935bf94>
2009-02-13 11:13:52 +0000

    Recorrido niveles (cont las clases de la cola) y resuelto ctor de copia

--- trunk/src/tad.cpp
+++ trunk/src/tad.cpp
@@ -5,24 +5,42 @@ using namespace std;
 
 int main ()
 {
-	TComplejo c1 (5,5), c2 (90,90), c3 (83,83), c4 (28,28), c5 (67,67), c6 (81,81), c7 (50,50), c8 (43,43), c9 (55,55), c10(45,45);
 	TAVLCom a;
-	TAVLCom b(a);
 	
-	a.Insertar (c1);
-	a.Insertar (c2);
-	a.Insertar (c3);
-	a.Insertar (c4);
-	a.Insertar (c5);
-	a.Insertar (c6);
-	a.Insertar (c7);
-	a.Insertar (c8);
-	a.Insertar (c9);
-	a.Insertar (c10);
+	for (int i=0; i<10000; i++)
+	{
+		TComplejo c (i,i);
+		
+		a.Insertar (c);
+	}
 	
-	cout << "Inordena: " << a.Inorden () << endl;
+	//~ cout << "Preorden: " << a.Preorden () << endl;
+	//~ cout << "Postorder: " << a.Postorden () << endl;
+	//~ cout << "Inorden: " << a.Inorden () << endl;
+	cout << "Niveles: " << a.Niveles () << endl;
 	
-	cout << "Inordenb: " << b.Inorden () << endl;
+	//~ TComplejo c1 (5,5), c2 (90,90), c3 (83,83), c4 (28,28), c5 (67,67),
+	//~ c6 (81,81), c7 (50,50), c8 (43,43), c9 (55,55), c10(45,45);
+	//~ TAVLCom a;
+	//~ 
+	//~ a.Insertar (c1);
+	//~ a.Insertar (c2);
+	//~ a.Insertar (c3);
+	//~ a.Insertar (c4);
+	//~ a.Insertar (c5);
+	//~ a.Insertar (c6);
+	//~ a.Insertar (c7);
+	//~ a.Insertar (c8);
+	//~ a.Insertar (c9);
+	//~ a.Insertar (c10);
+	//~ 
+	//~ TAVLCom b(a);
+	//~ 
+	//~ cout << "Inordena: " << a.Inorden () << endl;
+	//~ 
+	//~ cout << "Inordenb: " << b.Inorden () << endl;
+	//~ 
+	//~ cout << "Nivelesa: " << a.Niveles () << endl;
 	
 	//~ cout << "Nodos: " << a.Nodos () << endl;
 	
